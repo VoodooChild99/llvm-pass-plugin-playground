9bc64c0a0c203cfb2572762bfde1f976c22f4bca
Rayhub <1102290321@qq.com>
2024-07-02 07:40:12 +0000

    update config script

--- cmake-config.sh
+++ cmake-config.sh
@@ -2,12 +2,21 @@
 
 rm -rf build
 mkdir build
-cd build
+cd build || exit 1
 
-if [ -z "${LLVM_CONFIG}" ];
-then
+if [ -z "${LLVM_CONFIG}" ]; then
 	export LLVM_CONFIG=llvm-config
+	echo "[*] LLVM_CONFIG is not set, default to llvm-config"
 fi
 
-cmake	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
-			..
+if ! which "$LLVM_CONFIG" &> /dev/null; then
+	echo "[x] Failed to locate $LLVM_CONFIG"
+	exit 1
+fi
+
+if [ -z "$DCMAKE_BUILD_TYPE" ]; then
+	DCMAKE_BUILD_TYPE=RelWithDebInfo
+	echo "[*] DCMAKE_BUILD_TYPE is not set, default to RelWithDebInfo"
+fi
+
+cmake	-DCMAKE_BUILD_TYPE="$DCMAKE_BUILD_TYPE" ..
